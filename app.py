d84ff31a87be58f9cd21e18004600364b82f0d7d
jair555555 <92188759+jair555555@users.noreply.github.com>
2021-10-08 18:40:03 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__,template_folder='template') 
+
+@app.route('/') 
+def render():
+	return render_template('index.html')
+	
+@app.route('/productos')
+def productos():
+   return render_template('productos.html')
+
+
+
+app.run() #se encarga de ejecutar el servidor 5000
